6a3f431dae17005db989c47d97f5bdedd7550cda
Akshat Verma <112319520+Akshat162001@users.noreply.github.com>
2024-06-10 13:23:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import './index.css'
 import Program from './Components/Program/Program'
-
 function App() {
   return (
     <>
