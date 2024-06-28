51193d03cb4db083510953a81477f6f0ca515534
Akshat Verma <112319520+Akshat162001@users.noreply.github.com>
2024-06-28 09:20:30 +0530

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
 const Navbar = () => {
+  
   return (
     <nav className='container'>
       <img src={logo} alt="" className='logo'/>
